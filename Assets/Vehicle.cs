7d5a2f63b913d1a3db6eaecdeb1d63d73f1ebada
RoastedBard <roastedbard@gmail.com>
2020-09-02 00:49:36 +0300

    - Changes to the rotation

--- Assets/Vehicle.cs
+++ Assets/Vehicle.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class Vehicle : MonoBehaviour {
     Rigidbody rigidBody;
     AudioSource audioSource;
+    bool isOnTheGround = false;
     void Start() {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
 
     void Update() {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    void ProcessInput() {
+    void Rotate() {
+        if (isOnTheGround) return;
+        rigidBody.freezeRotation = true;
         if (Input.GetKey(KeyCode.A)) {
             transform.Rotate(Vector3.forward);
         } else if (Input.GetKey(KeyCode.D)) {
             transform.Rotate(Vector3.back);
         }
+        rigidBody.freezeRotation = false;
+    }
 
+    private void Thrust() {
         if (Input.GetKey(KeyCode.Space)) {
             rigidBody.AddRelativeForce(Vector3.up);
             if (!audioSource.isPlaying)
@@ -28,5 +35,13 @@ public class Vehicle : MonoBehaviour {
         } else {
             audioSource.Stop();
         }
+    }
+
+    void OnCollisionStay(Collision collisionInfo) {
+        isOnTheGround = true;
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        isOnTheGround = false;
     }
 }
