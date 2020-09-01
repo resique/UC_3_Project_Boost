7ea2dffa4b51920bded543a9beee5e8012000330
RoastedBard <roastedbard@gmail.com>
2020-09-01 19:25:05 +0300

    - Added thrusting

---
+++ Assets/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour {
+    Rigidbody rigidBody;
+
+    void Start() {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        ProcessInput();
+    }
+
+    void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space)) {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            print("Rotate Left");
+        } else if (Input.GetKey(KeyCode.D)) {
+            print("Rotate Right");
+        }
+    }
+}
